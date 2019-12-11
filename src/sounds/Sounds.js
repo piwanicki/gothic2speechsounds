83b25ef77f8db1632c1d2e53eb3db1a3aca0bcfc
piwanicki <pawel.iwanicki92@gmail.com>
2019-12-11 19:37:19 +0100

    add new local sounds

--- src/sounds/Sounds.js
+++ src/sounds/Sounds.js
@@ -1,29 +1,243 @@
-import sound1 from './SVM_4_SMALLTALK01.mp3'
-import sound2 from './SVM_4_SMALLTALK02.mp3'
-import sound3 from './SVM_4_SMALLTALK03.mp3'
-import sound4 from './SVM_4_SMALLTALK04.mp3'
-import sound5 from './SVM_4_SMALLTALK05.mp3'
-import sound6 from './SVM_4_SMALLTALK06.mp3'
-import sound7 from './SVM_4_SMALLTALK07.mp3'
-import sound8 from './SVM_4_SMALLTALK08.mp3'
-import sound9 from './SVM_4_SMALLTALK09.mp3'
-import sound10 from './SVM_4_SMALLTALK10.mp3'
+import sound1 from "./SVM_4_SMALLTALK01.mp3";
+import sound2 from "./SVM_4_SMALLTALK02.mp3";
+import sound3 from "./SVM_4_SMALLTALK03.mp3";
+import sound4 from "./SVM_4_SMALLTALK04.mp3";
+import sound5 from "./SVM_4_SMALLTALK05.mp3";
+import sound6 from "./SVM_4_SMALLTALK06.mp3";
+import sound7 from "./SVM_4_SMALLTALK07.mp3";
+import sound8 from "./SVM_4_SMALLTALK08.mp3";
+import sound9 from "./SVM_4_SMALLTALK09.mp3";
+import sound10 from "./SVM_4_SMALLTALK10.mp3";
+import sound11 from "./SVM_4_SMALLTALK11.mp3";
+import sound12 from "./SVM_4_SMALLTALK12.mp3";
+import sound13 from "./SVM_4_SMALLTALK13.mp3";
+import sound14 from "./SVM_4_SMALLTALK14.mp3";
+import sound15 from "./SVM_4_SMALLTALK15.mp3";
+import sound16 from "./SVM_4_SMALLTALK16.mp3";
+import sound17 from "./SVM_4_SMALLTALK17.mp3";
+import sound18 from "./SVM_4_SMALLTALK18.mp3";
+import sound19 from "./SVM_4_SMALLTALK19.mp3";
+import sound20 from "./SVM_4_SMALLTALK20.mp3";
+import sound21 from "./SVM_4_SMALLTALK21.mp3";
+import sound22 from "./SVM_4_SMALLTALK22.mp3";
+import sound23 from "./SVM_4_SMALLTALK23.mp3";
+import sound24 from "./SVM_4_SMALLTALK24.mp3";
+import sound25 from "./SVM_4_SMALLTALK25.mp3";
+import sound26 from "./SVM_4_SMALLTALK26.mp3";
+import sound27 from "./SVM_4_SMALLTALK27.mp3";
+import sound28 from "./SVM_4_SMALLTALK28.mp3";
+import sound29 from "./SVM_4_SMALLTALK29.mp3";
+import sound30 from "./SVM_4_SMALLTALK30.mp3";
+import sound31 from "./SVM_5_SMALLTALK01.mp3";
+import sound32 from "./SVM_5_SMALLTALK02.mp3";
+import sound33 from "./SVM_5_SMALLTALK03.mp3";
+import sound34 from "./SVM_5_SMALLTALK04.mp3";
+import sound35 from "./SVM_5_SMALLTALK05.mp3";
+import sound36 from "./SVM_5_SMALLTALK06.mp3";
+import sound37 from "./SVM_5_SMALLTALK07.mp3";
+import sound38 from "./SVM_5_SMALLTALK08.mp3";
+import sound39 from "./SVM_5_SMALLTALK09.mp3";
+import sound40 from "./SVM_5_SMALLTALK10.mp3";
+import sound41 from "./SVM_5_SMALLTALK11.mp3";
+import sound42 from "./SVM_5_SMALLTALK12.mp3";
+import sound43 from "./SVM_5_SMALLTALK13.mp3";
+import sound44 from "./SVM_5_SMALLTALK14.mp3";
+import sound45 from "./SVM_5_SMALLTALK15.mp3";
+import sound46 from "./SVM_5_SMALLTALK16.mp3";
+import sound47 from "./SVM_5_SMALLTALK17.mp3";
+import sound48 from "./SVM_5_SMALLTALK18.mp3";
+import sound49 from "./SVM_5_SMALLTALK19.mp3";
+import sound50 from "./SVM_5_SMALLTALK20.mp3";
+import sound51 from "./SVM_5_SMALLTALK21.mp3";
+import sound52 from "./SVM_5_SMALLTALK22.mp3";
+import sound53 from "./SVM_5_SMALLTALK23.mp3";
+import sound54 from "./SVM_5_SMALLTALK24.mp3";
+import sound55 from "./SVM_5_SMALLTALK25.mp3";
+import sound56 from "./SVM_5_SMALLTALK26.mp3";
+import sound57 from "./SVM_5_SMALLTALK27.mp3";
+import sound58 from "./SVM_5_SMALLTALK28.mp3";
+import sound59 from "./SVM_5_SMALLTALK29.mp3";
+import sound60 from "./SVM_5_SMALLTALK30.mp3";
+import sound61 from "./SVM_5_SMALLTALK01.mp3";
+import sound62 from "./SVM_5_SMALLTALK02.mp3";
+import sound63 from "./SVM_5_SMALLTALK03.mp3";
+import sound64 from "./SVM_5_SMALLTALK04.mp3";
+import sound65 from "./SVM_5_SMALLTALK05.mp3";
+import sound66 from "./SVM_5_SMALLTALK06.mp3";
+import sound67 from "./SVM_5_SMALLTALK07.mp3";
+import sound68 from "./SVM_5_SMALLTALK08.mp3";
+import sound69 from "./SVM_5_SMALLTALK09.mp3";
+import sound70 from "./SVM_5_SMALLTALK10.mp3";
+import sound71 from "./SVM_5_SMALLTALK11.mp3";
+import sound72 from "./SVM_5_SMALLTALK12.mp3";
+import sound73 from "./SVM_5_SMALLTALK13.mp3";
+import sound74 from "./SVM_5_SMALLTALK14.mp3";
+import sound75 from "./SVM_5_SMALLTALK15.mp3";
+import sound76 from "./SVM_5_SMALLTALK16.mp3";
+import sound77 from "./SVM_5_SMALLTALK17.mp3";
+import sound78 from "./SVM_5_SMALLTALK18.mp3";
+import sound79 from "./SVM_5_SMALLTALK19.mp3";
+import sound80 from "./SVM_5_SMALLTALK20.mp3";
+import sound81 from "./SVM_5_SMALLTALK21.mp3";
+import sound82 from "./SVM_5_SMALLTALK22.mp3";
+import sound83 from "./SVM_5_SMALLTALK23.mp3";
+import sound84 from "./SVM_5_SMALLTALK24.mp3";
+import sound85 from "./SVM_5_SMALLTALK25.mp3";
+import sound86 from "./SVM_5_SMALLTALK26.mp3";
+import sound87 from "./SVM_5_SMALLTALK27.mp3";
+import sound88 from "./SVM_5_SMALLTALK28.mp3";
+import sound89 from "./SVM_5_SMALLTALK29.mp3";
+import sound90 from "./SVM_5_SMALLTALK30.mp3";
+import sound91 from "./SVM_5_SMALLTALK01.mp3";
+import sound92 from "./SVM_5_SMALLTALK02.mp3";
+import sound93 from "./SVM_5_SMALLTALK03.mp3";
+import sound94 from "./SVM_5_SMALLTALK04.mp3";
+import sound95 from "./SVM_5_SMALLTALK05.mp3";
+import sound96 from "./SVM_5_SMALLTALK06.mp3";
+import sound97 from "./SVM_5_SMALLTALK07.mp3";
+import sound98 from "./SVM_5_SMALLTALK08.mp3";
+import sound99 from "./SVM_5_SMALLTALK09.mp3";
+import sound100 from "./SVM_5_SMALLTALK10.mp3";
+import sound101 from "./SVM_5_SMALLTALK11.mp3";
+import sound102 from "./SVM_5_SMALLTALK12.mp3";
+import sound103 from "./SVM_5_SMALLTALK13.mp3";
+import sound104 from "./SVM_5_SMALLTALK14.mp3";
+import sound105 from "./SVM_5_SMALLTALK15.mp3";
+import sound106 from "./SVM_5_SMALLTALK16.mp3";
+import sound107 from "./SVM_5_SMALLTALK17.mp3";
+import sound108 from "./SVM_5_SMALLTALK18.mp3";
+import sound109 from "./SVM_5_SMALLTALK19.mp3";
+import sound110 from "./SVM_5_SMALLTALK20.mp3";
+import sound111 from "./SVM_5_SMALLTALK21.mp3";
+import sound112 from "./SVM_5_SMALLTALK22.mp3";
+import sound113 from "./SVM_5_SMALLTALK23.mp3";
+import sound114 from "./SVM_5_SMALLTALK24.mp3";
+import sound115 from "./SVM_5_SMALLTALK25.mp3";
+import sound116 from "./SVM_5_SMALLTALK26.mp3";
+import sound117 from "./SVM_5_SMALLTALK27.mp3";
+import sound118 from "./SVM_5_SMALLTALK28.mp3";
+import sound119 from "./SVM_5_SMALLTALK29.mp3";
+import sound120 from "./SVM_5_SMALLTALK30.mp3";
 
 export const soundsURL = [
-   sound1,
-   sound2,
-   sound3,
-   sound4,
-   sound5,
-   sound6,
-   sound7,
-   sound8,
-   sound9,
-   sound10
-]
-
-
-
-
-
-
+  sound1,
+  sound2,
+  sound3,
+  sound4,
+  sound5,
+  sound6,
+  sound7,
+  sound8,
+  sound9,
+  sound10,
+  sound11,
+  sound12,
+  sound13,
+  sound14,
+  sound15,
+  sound16,
+  sound17,
+  sound18,
+  sound19,
+  sound20,
+  sound21,
+  sound22,
+  sound23,
+  sound24,
+  sound25,
+  sound26,
+  sound27,
+  sound28,
+  sound29,
+  sound30,
+  sound31, 
+sound32, 
+sound33, 
+sound34, 
+sound35, 
+sound36, 
+sound37, 
+sound38, 
+sound39, 
+sound40, 
+sound41, 
+sound42, 
+sound43, 
+sound44, 
+sound45, 
+sound46, 
+sound47, 
+sound48, 
+sound49, 
+sound50, 
+sound51, 
+sound52, 
+sound53, 
+sound54, 
+sound55, 
+sound56, 
+sound57, 
+sound58, 
+sound59, 
+sound60, 
+sound61, 
+sound62, 
+sound63, 
+sound64, 
+sound65, 
+sound66, 
+sound67, 
+sound68, 
+sound69, 
+sound70, 
+sound71, 
+sound72, 
+sound73, 
+sound74, 
+sound75, 
+sound76, 
+sound77, 
+sound78, 
+sound79, 
+sound80, 
+sound81, 
+sound82, 
+sound83, 
+sound84, 
+sound85, 
+sound86, 
+sound87, 
+sound88, 
+sound89, 
+sound90, 
+sound91, 
+sound92, 
+sound93, 
+sound94, 
+sound95, 
+sound96, 
+sound97, 
+sound98, 
+sound99, 
+sound100,
+sound101,
+sound102,
+sound103,
+sound104,
+sound105,
+sound106,
+sound107,
+sound108,
+sound109,
+sound110,
+sound111,
+sound112,
+sound113,
+sound114,
+sound115,
+sound116,
+sound117,
+sound118,
+sound119,
+sound120
+];
