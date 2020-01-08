0b7ebec0d3776e44b872d8c5243da4d21c5ceea2
piwanicki <pawel.iwanicki92@gmail.com>
2020-01-08 21:17:14 +0100

    some fixes

--- src/Components/SoundsComponent/SoundComponent.js
+++ src/Components/SoundsComponent/SoundComponent.js
@@ -1,34 +1,22 @@
-import React from 'react';
-import Sound from 'react-sound';
-import {soundsURL} from '../../assets/sounds/Sounds';
+import React from "react";
+import Sound from "react-sound";
+import { soundsURL } from "../../assets/sounds/Sounds";
 
 class SoundComponent extends React.Component {
-
-
-
-  finishedSonghandler = () => {
-
-  }
-
   render() {
-
-
-
     const smallTalkSex = this.props.smallTalk;
     const talkCategory = soundsURL[smallTalkSex];
-    console.log(talkCategory)
     const soundIDX = Math.floor(Math.random() * talkCategory.length);
-    return <Sound 
-      url={talkCategory[soundIDX]}
-      playStatus={this.props.playStatus}
-      autoLoad={true}
-      autoPlay={false}
-      onFinishedPlaying={this.props.onFinish}
-    />
+    return (
+      <Sound
+        url={talkCategory[soundIDX]}
+        playStatus={this.props.playStatus}
+        autoLoad={true}
+        autoPlay={false}
+        onFinishedPlaying={this.props.onFinish}
+      />
+    );
   }
-
-
 }
-
 
 export default SoundComponent;
