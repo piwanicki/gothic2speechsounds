bd99b5bf049b864fe47eb6dac083fea836ce82f7
piwanicki <pawel.iwanicki92@gmail.com>
2020-03-20 09:55:55 +0100

    add vatras speech

--- src/Components/SoundsComponent/SoundComponent.js
+++ src/Components/SoundsComponent/SoundComponent.js
@@ -3,32 +3,31 @@ import Sound from "react-sound";
 import { soundsURL } from "../../assets/sounds/Sounds";
 
 class SoundComponent extends React.Component {
-
-state = {
-  soundIDX: 0
-}
+  state = {
+    soundIDX: 0
+  };
 
   onFinishHandler = () => {
- 
     const category = this.props.category;
 
-    if(category === 'vatrasSpeech'){
-      const currentIdx =  this.state.soundIDX;
-      const idx = this.state.soundIDX === 20 ? 0 : currentIdx+1;
-      if(idx !== 0){
-      this.setState({
-        soundIDX: idx
-      })
+    if (category === "vatrasSpeech") {
+      const currentIdx = this.state.soundIDX;
+      const idx = this.state.soundIDX === 20 ? 0 : currentIdx + 1;
+      if (idx !== 0) {
+        this.setState({
+          soundIDX: idx
+        });
       }
-
     }
-  }
-
+  };
 
   render() {
     const category = this.props.category;
     const soundCategory = soundsURL[category];
-    let soundIDX = this.props.category === 'vatrasSpeech' ? this.state.soundIDX : Math.floor(Math.random() * soundCategory.length);
+    let soundIDX =
+      this.props.category === "vatrasSpeech"
+        ? this.state.soundIDX
+        : Math.floor(Math.random() * soundCategory.length);
     return (
       <Sound
         url={soundCategory[soundIDX]}
@@ -36,7 +35,6 @@ state = {
         autoLoad={true}
         autoPlay={false}
         onFinishedPlaying={this.onFinishHandler}
-        
       />
     );
   }
